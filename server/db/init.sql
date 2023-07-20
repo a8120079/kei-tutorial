730ff43a3a3d9a941eaddacba1949104cb7961ff
FantiGA <98534535+FantiGA@users.noreply.github.com>
2023-07-20 23:36:52 +0900

    Create init.sql

---
+++ server/db/init.sql
@@ -0,0 +1,17 @@
+-- 测试数据
+
+INSERT INTO 'games' ('level', 'init_script', 'exec_script') VALUES (1, '[[4,3,true],[4,4,false]]', '[[4,3,true]]');
+INSERT INTO 'games' ('level', 'init_script', 'exec_script') VALUES (1, '[[4,3,true],[4,4,false]]', '[[4,3,true]]');
+INSERT INTO 'games' ('level', 'init_script', 'exec_script') VALUES (1, '[[4,3,true],[4,4,false]]', '[[4,3,true]]');
+INSERT INTO 'games' ('level', 'init_script', 'exec_script') VALUES (1, '[[4,3,true],[4,4,false]]', '[[4,3,true]]');
+INSERT INTO 'games' ('level', 'init_script', 'exec_script') VALUES (1, '[[4,3,true],[4,4,false]]', '[[4,3,true]]');
+INSERT INTO 'games' ('level', 'init_script', 'exec_script') VALUES (1, '[[4,3,true],[4,4,false]]', '[[4,3,true]]');
+INSERT INTO 'games' ('level', 'init_script', 'exec_script') VALUES (1, '[[4,3,true],[4,4,false]]', '[[4,3,true]]');
+INSERT INTO 'games' ('level', 'init_script', 'exec_script') VALUES (1, '[[4,3,true],[4,4,false]]', '[[4,3,true]]');
+INSERT INTO 'games' ('level', 'init_script', 'exec_script') VALUES (1, '[[4,3,true],[4,4,false]]', '[[4,3,true]]');
+INSERT INTO 'games' ('level', 'init_script', 'exec_script') VALUES (1, '[[4,3,true],[4,4,false]]', '[[4,3,true]]');
+INSERT INTO 'games' ('level', 'init_script', 'exec_script') VALUES (1, '[[4,3,true],[4,4,false]]', '[[4,3,true]]');
+INSERT INTO 'games' ('level', 'init_script', 'exec_script') VALUES (1, '[[4,3,true],[4,4,false]]', '[[4,3,true]]');
+INSERT INTO 'games' ('level', 'init_script', 'exec_script') VALUES (1, '[[4,3,true],[4,4,false]]', '[[4,3,true]]');
+INSERT INTO 'games' ('level', 'init_script', 'exec_script') VALUES (1, '[[4,3,true],[4,4,false]]', '[[4,3,true]]');
+INSERT INTO 'games' ('level', 'init_script', 'exec_script') VALUES (1, '[[4,3,true],[4,4,false]]', '[[4,3,true]]');
