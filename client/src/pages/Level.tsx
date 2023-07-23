9c2cff649d76895ffdd15cd3b1a0c006af5f2ad5
a8120079 <103491452+a8120079@users.noreply.github.com>
2023-07-23 12:46:49 +0900

    db直した

--- client/src/pages/Level.tsx
+++ client/src/pages/Level.tsx
@@ -62,41 +62,84 @@ const Level: FC = () => {
               </List>
             </AccordionDetails>
           </Accordion>
-          <Accordion>
+
+          <Accordion defaultExpanded={true}>
             <AccordionSummary
               expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel2a-content"
-              id="panel2a-header"
+              aria-controls="panel1a-content"
+              id="panel1a-header"
             >
-              <Typography>初級レベル</Typography>
+              <Typography>入門レベル</Typography>
             </AccordionSummary>
             <AccordionDetails>
-              <Typography>
-                <Link href="#" variant="button" onClick={() => navigate('/Game')}>B - 1</Link>
-                <Link href="#" variant="button" onClick={() => navigate('/Game')}>B - 2</Link>
-                <Link href="#" variant="button" onClick={() => navigate('/Game')}>B - 3</Link>
-                <Link href="#" variant="button" onClick={() => navigate('/Game')}>B - 4</Link>
-                <Link href="#" variant="button" onClick={() => navigate('/Game')}>B - 5</Link>
-              </Typography>
+              <List disablePadding>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary="B - 1" onClick={() => navigate('/Game')} />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary="B - 2" onClick={() => navigate('/Game')} />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary="B - 3" onClick={() => navigate('/Game')} />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary="B - 4" onClick={() => navigate('/Game')} />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary="B - 5" onClick={() => navigate('/Game')} />
+                  </ListItemButton>
+                </ListItem>
+              </List>
+            </AccordionDetails>
+          </Accordion>          
+          <Accordion>
+          <Accordion defaultExpanded={true}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>入門レベル</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <List disablePadding>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary="C - 1" onClick={() => navigate('/Game')} />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary="C - 2" onClick={() => navigate('/Game')} />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary="C - 3" onClick={() => navigate('/Game')} />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary="C - 4" onClick={() => navigate('/Game')} />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary="C - 5" onClick={() => navigate('/Game')} />
+                  </ListItemButton>
+                </ListItem>
+              </List>
             </AccordionDetails>
           </Accordion>
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel3a-content"
-              id="panel3a-header"
-            >
-              <Typography>中級レベル</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                <Link href="#" variant="button" onClick={() => navigate('/Game')}>C - 1</Link>
-                <Link href="#" variant="button" onClick={() => navigate('/Game')}>C - 2</Link>
-                <Link href="#" variant="button" onClick={() => navigate('/Game')}>C - 3</Link>
-                <Link href="#" variant="button" onClick={() => navigate('/Game')}>C - 4</Link>
-                <Link href="#" variant="button" onClick={() => navigate('/Game')}>C - 5</Link>
-              </Typography>
-            </AccordionDetails>
           </Accordion>
         </Grid>
       </Grid>
